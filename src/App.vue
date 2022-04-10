ad35ac4dec0a48e8dd8270050dcd7b63074d8c05
Łukasz Głaz <lukasz.glaz@gmail.com>
2022-04-10 15:26:07 +0200

    Inbox Screen

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
+<script setup>
+import InboxScreen from './components/InboxScreen.vue';
+</script>
+
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <InboxScreen />
   </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
-</script>
 
 <style>
 @import "./index.css";
---
+++ src/components/InboxScreen.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useStore } from 'vuex';
+import PureInboxScreen from './PureInboxScreen';
+import {computed} from "vue";
+
+const store = useStore();
+const error = computed(() => store.state.error);
+
+</script>
+
+<template>
+  <PureInboxScreen :error="error" />
+</template>
