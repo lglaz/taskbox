a51988a9b0f0d53cdef9e39ef48c55c7d48405ea
Łukasz Głaz <lukasz.glaz@gmail.com>
2022-04-10 09:57:17 +0200

    Task component

---
+++ src/components/Task.vue
@@ -0,0 +1,47 @@
+<script setup>
+import {computed, defineProps, defineEmit} from "vue";
+
+const emit = defineEmit(['archive-task', 'pin-task'])
+
+const props = defineProps({task: {
+    type: Object,
+    required: true,
+    default: () => ({id: '', state: '', title: ''}),
+    validator: task => ['id', 'state', 'title'].every(key => key in task),
+  }
+});
+
+const classes = computed(() => ({
+  'list-item TASK_INBOX': props.task.state === 'TASK_INBOX',
+  'list-item TASK_PINNED': props.task.state === 'TASK_PINNED',
+  'list-item TASK_ARCHIVED': props.task.state === 'TASK_ARCHIVED',
+}));
+
+const isChecked = computed(() => props.task.state === 'TASK_ARCHIVED');
+
+function archiveTask() {
+  emit('archive-task', props.task.id);
+}
+
+function pinTask() {
+  emit('pin-task', props.task.id);
+}
+
+</script>
+
+<template>
+  <div :class="classes">
+    <label class="checkbox">
+      <input type="checkbox" :checked="isChecked" disabled name="checked" />
+      <span class="checkbox-custom" @click="archiveTask()" :aria-label="'archiveTask-' + task.id" />
+    </label>
+    <div class="title">
+      <input type="text" :value="task.title" readonly placeholder="Input title" />
+    </div>
+    <div class="actions">
+      <a v-if="!isChecked" @click="pinTask()">
+        <span class="icon-star" :aria-label="'pinTask-' + task.id" />
+      </a>
+    </div>
+  </div>
+</template>
